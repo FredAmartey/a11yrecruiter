7c909135e7c7fbb586cc65e3c24713f15412ae9a
FredAmartey <fdamartey@gmail.com>
2019-04-01 16:10:50 -0400

    remove googleAuth key from repo, should be private

--- auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	'googleAuth' : {
-		'clientID' : '776128651824-b2btrasqv8a50tn32jed3946d5jfkmpd.apps.googleusercontent.com',
-		'clientSecret' : 'hEk4jUKtXYaSuQ7pGzwmLATU',
-		'callbackURL' : 'https://a11yrecruiter.com/auth/google/callback'
-	}
-}
