018c40e38f26cf657616788fe2b4dd22458681ef
FredAmartey <fdamartey@gmail.com>
2019-04-01 15:48:24 -0400

    updated user schema

---
+++ models/jobs.js
@@ -0,0 +1,13 @@
+var mongoose    = require("mongoose");
+
+var jobSchema = new mongoose.Schema({
+    title: String,
+    company:String,
+    type: String,
+    location: String,
+    description: String,
+    requirements: String,
+    applydate: String
+});
+
+module.exports = mongoose.model("Job", jobSchema);
--- models/user.js
+++ models/user.js
@@ -3,9 +3,18 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
-})
+	google: {
+		id: String,
+		token: String,
+		email: String,
+		name: String
+	},
+	    firstName: String,
+	    lastName: String,
+	    email: String,
+	    password: String
+
+}); 
 
 UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User", UserSchema);
