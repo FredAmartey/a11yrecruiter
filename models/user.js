95c028a17d3974b37135e7497474f6d568a38949
FredAmartey <fdamartey@gmail.com>
2019-05-29 13:19:51 -0400

    User and Jobs Schemas

--- models/user.js
+++ models/user.js
@@ -1,20 +1,22 @@
-var mongoose = require("mongoose");
-var passportLocalMongoose = require("passport-local-mongoose");
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 var UserSchema = new mongoose.Schema({
 	google: {
 		id: String,
 		token: String,
-		email: String,
-		name: String
+		email: { type: String, require: true, index:true, unique:true,sparse:true},
+		fullName: String,
+		firstName: String,
+		lastName:String
 	},
 	    firstName: String,
 	    lastName: String,
-	    email: String,
-	    password: String
+	    email: { type: String, require: true, index:true, unique:true,sparse:true},
+	    password: { type: String, require:true }
 
-}); 
+});
 
 UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User", UserSchema);
